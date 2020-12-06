0ac3abd2993207b9ba2c99459172be5122181272
Hugo MANCINI <h.g.mancini@gmail.com>
2020-12-06 21:19:00 +0100

    correctmail

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -1,6 +1,6 @@
 class ContactFormMailer < ApplicationMailer
   def contact_form_mail
       @contact = params[:contact]
-      mail(to: "contact@solutioncreance.com", subject: "Nouveau contact !")
+      mail(to: "contact@solutioncreance.fr", subject: "Nouveau contact !")
     end
 end
