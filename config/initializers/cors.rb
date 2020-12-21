d85e1bdd017bc5d3f30dfae34c64eb577e8a1292
Hugo MANCINI <h.g.mancini@gmail.com>
2020-12-21 15:31:47 +0100

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'http://www.solutioncreance.fr'
+    origins 'https://solutioncreance.fr'
 
     resource '*',
       headers: :any,
