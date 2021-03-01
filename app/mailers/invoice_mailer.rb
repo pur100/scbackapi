d32357cb4607b1df28cff0cdb489555f93efc270
Hugo MANCINI <h.g.mancini@gmail.com>
2021-03-01 15:25:28 +0100

    mail lény

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,6 +1,6 @@
 class InvoiceMailer < ApplicationMailer
   def invoice_mail
       @invoice = params[:invoice]
-      mail(to: "h.g.mancini@gmail.com", subject: "Nouvelle facture en ligne")
+      mail(to: "contact@solutioncreance.fr", subject: "Nouvelle facture en ligne")
     end
 end
